2f4d47a6789ab2c8e3c2e08dff64cdf2ff3b04bd
HEROggwp <andrecbezerra29.02@gmail.com>
2025-11-28 18:01:02 -0300

    inimigos

--- TCC-jogo/index.js
+++ TCC-jogo/index.js
@@ -1,8 +1,30 @@
 const player = document.getElementById("player");
-const janela = document.querySelector(".janela");
+const janela = document.querySelector('.janela');
+const janelaWidth = janela.offsetWidth;
+const janelaHeight = janela.offsetHeight;
 
-let x = (janela.clientWidth - player.clientWidth) / 2;
-let y = (janela.clientHeight - player.clientHeight) / 2;
+function createEnemy() {
+    const enemy = document.createElement('div');
+    enemy.classList.add('enemy');
+
+    const randomX = Math.floor(Math.random() * (janelaWidth - 30));
+    const randomY = Math.floor(Math.random() * (janelaHeight - 30));
+
+    enemy.style.left = `${randomX}px`;
+    enemy.style.top = `${randomY}px`;
+
+    // Adiciona o inimigo à área do jogo
+    janela.appendChild(enemy);
+
+    // Opcional: Remove o inimigo após alguns segundos
+    setTimeout(() => {
+        enemy.remove();
+    }, 500); // Inimigo desaparece após 3 segundos
+}
+
+
+let x = (janela.clientWidth - player.clientWidth) ;
+let y = (janela.clientHeight - player.clientHeight) ;
 
 let speed = 4;
 let keys = {};
@@ -29,6 +51,9 @@ function gameLoop() {
     // Atualizar posição real
     player.style.left = x + "px";
     player.style.top = y + "px";
+
+    // inimigo de gelo// 
+    setInterval(createEnemy, 9000);
 
     requestAnimationFrame(gameLoop);
 }
